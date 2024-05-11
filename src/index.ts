f9073915f1647711ba383ea63e4cd98c5cf2bf33
TabindaNoor <tabindanoor415@gmail.com>
2024-05-12 00:09:15 +0500

    restaurant

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,23 @@ import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
 import myUserRoute from "./routes/MyUserRoute"
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 import { Request, Response } from "express"
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose
         .connect(process.env.DATABASE_STRING as string)
         .then(()=>{console.log("Connected to the database!")
 })
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+
 
 const app = express()
 app.use(express.json())
@@ -24,6 +35,8 @@ app.get("/health", async (req:Request, res:Response)=>{
 })
 
 app.use("/api/my/user",myUserRoute)
+app.use("/api/my/restaurant",myRestaurantRoute)
+
 
 app.listen(7000,()=>{
     console.log("listening at port 7000...")
